fd5ec751bfc8bcf84dbac36d18afbc246a6deaa6
Daniel Vinntreus <gudski@gmail.com>
2014-02-06 21:31:14 +0100

    Add mocha+chai and first passing test
    Test does not actually do anything yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,24 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    mochaTest : {
+      test: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true
+        },
+        src: ['test/**/*.js']
+      },
+    },
     jshint: {
         all: ['Gruntfile.js', './*.js', 'test/**/*.js', 'lib/**/*.js', 'routes/**/*.js']
       }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
---
+++ test/index_tests.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect;
+var app = require("../app.js");
+
+describe('index', function () {
+	describe('GET', function () {
+		it('should return html', function () {
+			
+		});
+	});	
+});
