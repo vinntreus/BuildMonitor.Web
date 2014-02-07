64dde65c63e7efac1a21fb6c64c47ee143ba3390
Daniel Vinntreus <gudski@gmail.com>
2014-02-07 23:40:03 +0100

    Add sample graph using 'dygraph'

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
-
+var moment = require("moment");
 /*
  * GET home page.
  */
 
 exports.index = function(db){
-    var buildData = db.getBuildData();
+    var buildData = db.getBuildData().map(function(item){
+      return [
+        moment(item.Start).format(),
+        item.Time
+      ];
+    });
+    console.log(buildData);
     return function(req, res){
         res.render('index', { title: 'Express', buildData : buildData });
     };  
