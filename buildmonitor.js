7b6e5b7346cd3dd0bfa7c9d0200ca7e97503b45c
Peter Örneholm <peter@orneholm.com>
2014-08-03 23:09:27 +0200

    First working version of the web analyzer

---
+++ buildmonitor.js
@@ -0,0 +1,130 @@
+﻿(function (window) {
+    var document = window.document,
+        wrapperElement = document.getElementsByClassName('buildmonitor')[0],
+        inputElement = wrapperElement.getElementsByTagName('textarea')[0],
+        outputElement = wrapperElement.getElementsByClassName('stats')[0],
+        chartElement = wrapperElement.getElementsByClassName('chart')[0],
+        chartHeader = chartElement.getElementsByTagName('header')[0],
+        chartCanvasElement = chartElement.getElementsByTagName('canvas')[0],
+        worker;
+
+    chartHeader.style.display = 'none';
+
+    inputElement.addEventListener('input', function () {
+        render(JSON.parse(this.value));
+    });
+
+    function render(buildtimes) {
+        calculateStatsInWorker(buildtimes, function (result) {
+            var stats = result.projects.stats,
+                statsHtml = getStatsHtml(stats),
+                lineChartData = getLineChartData(stats),
+                canvasContext = chartCanvasElement.getContext('2d');
+
+            new Chart(canvasContext).Line(lineChartData, {
+                responsive: true,
+                maintainAspectRatio: false,
+                bezierCurve: false
+            });
+            chartHeader.style.display = 'block';
+
+            outputElement.innerHTML = statsHtml;
+        });
+    }
+
+    function calculateStatsInWorker(buildtimes, callback) {
+        worker = worker || new Worker('buildmonitor.stats.worker.js');
+        worker.addEventListener('message', function (event) {
+            callback(event.data);
+        });
+        worker.postMessage(buildtimes);
+    }
+
+    function getStatsHtml(stats) {
+        var html = [];
+
+        html.push('<table>');
+        html.push('<thead>');
+        html.push('<tr>');
+        html.push('<th>Date</th>');
+        html.push('<th>Builds</th>');
+        html.push('<th>Time</th>');
+        html.push('</tr>');
+        html.push('</thead>');
+
+
+        html.push('<tbody>');
+
+        Object.keys(stats.totalTimeByDate).forEach(function (year) {
+            Object.keys(stats.totalTimeByDate[year]).forEach(function (month) {
+                html.push('<tr>');
+                html.push('<th>' + getDateDisplayName(month - 1, year) + '</th>');
+                html.push('<td>' + 'N/A' + '</td>');
+                html.push('<td>' + getTimeDisplayName(stats.totalTimeByDate[year][month]) + '</td>');
+                html.push('</tr>');
+            });
+        });
+
+        html.push('</tbody>');
+
+        html.push('<tfoot>');
+        html.push('<tr>');
+        html.push('<th>Total</th>');
+        html.push('<td>' + stats.totalRun + '</td>');
+        html.push('<td>' + getTimeDisplayName(stats.totalTime) + '</td>');
+        html.push('</tr>');
+        html.push('</tfoot>');
+        html.push('</table>');
+
+        return html.join('');
+
+        function getTimeDisplayName(milliseconds) {
+            var seconds = milliseconds / 1000,
+                minutes = seconds / 60,
+                roundedMinutes = Math.round(minutes),
+                roundedMinutesWithUnit = roundedMinutes + ' min';
+
+            return !!milliseconds ? roundedMinutesWithUnit : '-';
+        }
+
+        function getDateDisplayName(month, year) {
+            var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            return monthNames[month] + '/' + year;
+        }
+    }
+
+    function getLineChartData(stats) {
+        var lineChartData = {
+            labels: [],
+            datasets: []
+        },
+            dataset = {
+                label: 'Build times',
+                fillColor: 'hsla(126, 45%, 66%, 0.2)',
+                strokeColor: 'hsla(126, 45%, 66%, 1)',
+                pointColor: 'hsla(126, 45%, 66%, 1)',
+                pointStrokeColor: '#fff',
+                pointHighlightFill: '#000',
+                pointHighlightStroke: 'hsla(126, 45%, 66%, 1)',
+                data: []
+            },
+            monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+        Object.keys(stats.totalTimeByDate).forEach(function (year) {
+            Object.keys(stats.totalTimeByDate[year]).forEach(function (month) {
+                var milliseconds = stats.totalTimeByDate[year][month],
+                    seconds = milliseconds / 1000,
+                    minutes = seconds / 60,
+                    roundedMinutes = Math.round(minutes),
+                    label = monthNames[month - 1] + '/' + year;
+
+                lineChartData.labels.push(label);
+                dataset.data.push(roundedMinutes);
+            });
+        });
+
+        lineChartData.datasets.push(dataset);
+
+        return lineChartData;
+    }
+}(this));
---
+++ buildmonitor.stats.worker.js
@@ -0,0 +1,126 @@
+﻿(function (global) {
+    global.addEventListener('message', function(event) {
+        var stats = getSolutionAndProjectStats(event.data);
+        global.postMessage(stats);
+    });
+
+    function getSolutionAndProjectStats(buildtimes) {
+        var solutions = buildtimes,
+            solutionsCollection = getItemsCollection(solutions, 'Solution', 'Name'),
+            projects = buildtimes.reduce(function (a, b) {
+                return a.concat(b.Projects);
+            }, []),
+            projectsCollection = getItemsCollection(projects, 'Project', 'Id');
+
+        return {
+            solutions: updateStats(solutionsCollection),
+            projects: updateStats(projectsCollection)
+        };
+    }
+
+    function getItemsCollection(entries, entryKey, itemIdKey) {
+        var itemsMap = {},
+            items = [];
+
+        entries.forEach(function (entry) {
+            var item = entry[entryKey],
+                itemId;
+
+            if (!item) {
+                return;
+            }
+
+            itemId = item[itemIdKey];
+
+            if (!itemsMap[itemId]) {
+                itemsMap[itemId] = {
+                    name: item.Name,
+                    events: []
+                };
+                items.push(itemsMap[itemId]);
+            }
+
+            itemsMap[itemId].events.push({
+                start: entry.Start,
+                time: entry.Time
+            });
+        });
+
+        return {
+            items: items
+        };
+    }
+
+    function updateStats(itemsCollection) {
+        itemsCollection.items.forEach(function (item) {
+            item.stats = getItemStats(item.events);
+        });
+
+        itemsCollection.stats = getItemsStats(itemsCollection.items);
+
+        return itemsCollection;
+    }
+
+    function getItemStats(events) {
+        var totalRun = 0,
+            totalTime = 0,
+            totalTimeByDate = {};
+
+        events.forEach(function (event) {
+            var start = new Date(event.start),
+                year = start.getFullYear(),
+                month = start.getMonth() + 1;
+
+            totalRun += 1;
+            totalTime += event.time;
+
+            if (!totalTimeByDate[year]) {
+                totalTimeByDate[year] = {};
+                for (var i = 1; i <= 12; i++) {
+                    totalTimeByDate[year][i] = 0;
+                }
+            }
+
+            totalTimeByDate[year][month] += event.time;
+        });
+
+        return {
+            totalRun: totalRun,
+            totalTime: totalTime,
+            averageTime: totalTime / totalRun,
+            totalTimeByDate: totalTimeByDate
+        };
+    }
+
+    function getItemsStats(items) {
+        var totalRun = 0,
+            totalTime = 0,
+            totalTimeByDate = {},
+            i = 0;
+
+        items.forEach(function (item) {
+            totalRun += item.stats.totalRun;
+            totalTime += item.stats.totalTime;
+
+            Object.keys(item.stats.totalTimeByDate).forEach(function (year) {
+                if (!totalTimeByDate[year]) {
+                    totalTimeByDate[year] = {};
+                    for (i = 1; i <= 12; i++) {
+                        totalTimeByDate[year][i] = 0;
+                    }
+                }
+
+                for (i = 1; i <= 12; i++) {
+                    totalTimeByDate[year][i] += item.stats.totalTimeByDate[year][i];
+                }
+            });
+        });
+
+        return {
+            totalRun: totalRun,
+            totalTime: totalTime,
+            averageTime: totalTime / totalRun,
+            totalTimeByDate: totalTimeByDate
+        };
+    }
+}(this));
